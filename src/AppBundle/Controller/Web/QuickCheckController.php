c63323a60eeefc60c74354d2f63ff94157191eb9
Alexey Kruchenok <alexey.kruchenok@gmail.com>
2017-07-24 19:51:34 +0300

    validated

--- src/AppBundle/Controller/Web/QuickCheckController.php
+++ src/AppBundle/Controller/Web/QuickCheckController.php
@@ -42,24 +42,84 @@ class QuickCheckController extends Controller
 
         $base_template_content = file_get_contents($baseTemplate);
 
-        file_put_contents($templateFile, $baseTemplate."\n".$template);
+        file_put_contents($templateFile, $base_template_content."\n".$template);
 
-        $command = "cd $pScript && $pPython $pScript/render.py -DW $tmpDir -DT $templateDir -v -t quickcheck";
+        $combined_template_content = file_get_contents($templateFile);
 
-        exec($command, $output);
-        $out_finished = '';
-        $brCount = 0;
-        foreach($output as $line) {
-            $out_finished .= $line . "<br>";
-            $brCount++;
+        $command_validate = "cd $pScript && $pPython $pScript/render.py -DW $tmpDir -DT $templateDir -v -t quickcheck";
+
+
+        exec($command_validate, $output_validate);
+
+        $validate_ok = false;
+
+        $out_validate_finished = '';
+
+
+        foreach($output_validate as $line) {
+            if(strpos($line, 'TemplateRenderException:') === false)
+            {
+                // do nothing, wierd logic when match at 0 position != false is true, but === false is false
+            }
+            else {
+                $linenum = $this->getLineNumber($line);
+                $out_validate_finished .= '>'.$this->getLine($combined_template_content, $linenum)."\n";
+                $out_validate_finished .= $this->getStripedError($line) . "\n";
+            }
+        }
+
+        $out_finished = 'ERROR';
+
+
+        if(strlen($out_validate_finished) == 0)
+        {
+            $out_finished = '';
+            $command = "cd $pScript && $pPython $pScript/render.py -DW $tmpDir -DT $templateDir -t quickcheck";
+
+            exec($command, $output);
+
+            $brCount = 0;
+            foreach($output as $line) {
+                $out_finished .= $line . "\n";
+                $brCount++;
+            }
         }
 
 
         $params = [];
         $params['template'] = $template;
         $params['result'] = $out_finished;
+        $params['result_validation'] = $out_validate_finished;
+
         $params['base_dir'] = realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR;
 
         return $this->render('quickcheck/result.html.twig', $params);
+    }
+
+    function getLineNumber($line)
+    {
+        $bpos = strpos($line, '(')+1;
+        $epos = strpos($line, ':', $bpos);
+
+        $sub = substr($line,$bpos,$epos-$bpos);
+
+        return intval($sub);
+    }
+
+    function getStripedError($line)
+    {
+        $bpos = 0;
+        $epos = strpos($line, ' in file ', $bpos);
+
+        $sub = substr($line,$bpos,$epos-$bpos);
+
+        return $sub;
+    }
+
+
+    function getLine($text, $linenumber)
+    {
+        $lines = split("\n",$text);
+        return $lines[$linenumber-1];
     }
 }
