77bfc969db16e264d2acfd8d4f6a5009dc3894ab
Alexey Kruchenok <alexey.kruchenok@gmail.com>
2018-03-20 01:58:45 +0300

    assembl

---
+++ bin/imagebuild.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+s2i build git@gitlab.com:robobloglab/sattelitor-services.git hydrargentum/php71-cli-centos7-base satterlitor-services
---
+++ bin/imagepush.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker tag text-generator-service registry.gitlab.com/robobloglab/text-generator-service
+docker push registry.gitlab.com/robobloglab/text-generator-service
