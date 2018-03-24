1703a71fb1dc1ee665ff0d52b615e9ab91f56a84
Alexey Kruchenok <alexey.kruchenok@gmail.com>
2018-03-25 02:38:11 +0300

    nginx config

---
+++ bin/getsapi.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+http GET https://sapi.robobloglab.com/frontapi/v1/getuserinfo \
+     "Authorization:Bearer YTgyMTJhY2VmNTBlZWRlZjFjZGM1ODYxMmNjMDYyMzVjMTc1NjRiYWU1YTNhZGFiNzcyNDdkZWViOGJlM2NhNA" \
+     --verify=no
