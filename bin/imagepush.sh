7401a22e607202f95bc2be4a7bf045a424cbb5b4
Alexey Kruchenok <alexey.kruchenok@gmail.com>
2018-04-04 14:08:05 +0300

    buildsystem improve

--- bin/imagepush.sh
+++ bin/imagepush.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker tag text-generator-service registry.gitlab.com/robobloglab/text-generator-service
-docker push registry.gitlab.com/robobloglab/text-generator-service
+docker tag satterlitor-services registry.gitlab.com/robobloglab/satterlitor-services
+docker push registry.gitlab.com/robobloglab/satterlitor-services
