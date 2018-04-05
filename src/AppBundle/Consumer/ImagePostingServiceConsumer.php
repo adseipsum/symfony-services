e5e49b198ddcc6b1c34098541e6e8ad28f1fecbc
Oleksiy <oleksiy.perepelytsya@gmail.com>
2018-04-05 21:03:33 +0000

    lost consumers

---
+++ src/AppBundle/Consumer/ImagePostingServiceConsumer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Consumer;
+
+use AppBundle\Extension\ImagePostingServiceExtension;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+class ImagePostingServiceConsumer implements ConsumerInterface
+{
+    private $container;
+
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    /**
+     * @var AMQPMessage $msg
+     * @return void
+     */
+    public function execute(AMQPMessage $msg)
+    {
+        $cb = $this->container->get('couchbase.connector');
+        $amqp = $this->container->get('old_sound_rabbit_mq.image_posting_service_producer');
+
+        $imagePostingService = new ImagePostingServiceExtension($cb, $amqp);
+
+        $imagePostingService->processMessage($msg);
+    }
+}
---
+++ src/AppBundle/Consumer/PostManagerServiceConsumer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Consumer;
+
+use AppBundle\Extension\PostManagerServiceExtension;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+class PostManagerServiceConsumer implements ConsumerInterface
+{
+    private $container;
+
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    /**
+     * @var AMQPMessage $msg
+     * @return void
+     */
+    public function execute(AMQPMessage $msg)
+    {
+        $cb = $this->container->get('couchbase.connector');
+        $amqp = $this->container->get('old_sound_rabbit_mq.post_manager_service_producer');
+
+        $postManager = new PostManagerServiceExtension($cb, $amqp);
+
+        $postManager->processMessage($msg);
+    }
+}
